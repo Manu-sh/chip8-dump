8330ef8de28a8200bf5ede9a17d37493dde1a478
Manu-Sh <s3gmentationfault@gmail.com>
2025-10-31 23:07:35 +0100

    first commit

---
+++ dump-chip8.c
@@ -0,0 +1,311 @@
+#define _DEFAULT_SOURCE
+#include <endian.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <asm/byteorder.h>
+
+// helper to cut away the padding bits: data >> (16 - to_extract));
+// this is like the ''.substr(0, bit_length) method but for binary,
+// valid ranges for bit_length is 1-16
+// take_few_bits(0b11110101, 4) -> 1111
+// take_few_bits(0b10110101, 4) -> 1011
+uint16_t take_few_bits(uint16_t data, uint8_t bit_length) {
+    assert(bit_length <= 16);
+    return data >> (16 - bit_length);
+}
+
+uint16_t bit_slice(uint16_t data, uint8_t from, uint8_t to) {
+    return take_few_bits(data << from, to) >> from;
+}
+
+uint16_t nibble_slice(uint16_t data, uint8_t from, uint8_t to) {
+    return bit_slice(data, from * 4, to * 4);
+}
+
+typedef union  __attribute__((__packed__)) {
+
+    uint16_t data;
+    uint8_t byte[sizeof(uint16_t)];
+
+	struct {
+        #ifdef __LITTLE_ENDIAN_BITFIELD
+            uint16_t subtype : 4;
+            uint16_t : 4;
+            uint16_t : 4;
+            uint16_t type : 4;
+        #elif defined(__BIG_ENDIAN_BITFIELD)
+            uint16_t type : 4;
+            uint16_t : 4;
+            uint16_t : 4;
+            uint16_t subtype : 4;
+        #else
+            #error "ooops"
+        #endif
+	};
+
+} opcode_t;
+
+
+void dump_instruction(opcode_t instr) {
+
+    static size_t instruction_counter;
+
+    printf("%x\n", instr.type);
+
+    assert(instr.type == nibble_slice(instr.data, 0, 1));
+
+    printf("[%zu] ", ++instruction_counter);
+
+    if (instr.data == 0x00E0)
+        return (void)printf("%#06X disp_clear() - Clears the screen\n", instr.data);
+    else if (instr.data == 0x00EE)
+        return (void)printf("%#06X return; - Returns from a subroutine.\n", instr.data);
+
+    switch (instr.type) {
+        case 0:
+            return (void)printf("%#06X call( %#03X ); - Calls machine code routine at address NNN.\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 4)
+            );
+        case 1:
+            return (void)printf("%#06X goto %#03X; - Jumps to address NNN.\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 4)
+            );
+        case 2:
+            return (void)printf("%#06X *(%#03X)() - Calls subroutine at NNN.\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 4)
+            );
+        case 3:
+            return (void)printf("%#06X if (V%x == %#02x) - Skips the next instruction if VX equals NN (usually the next instruction is a jump to skip a code block).\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 2),
+               nibble_slice(instr.data, 2, 4)
+            );
+        case 4:
+            return (void)printf("%#06X if (V%x != %#02x) - Skips the next instruction if VX does not equal NN (usually the next instruction is a jump to skip a code block).\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 2),
+                nibble_slice(instr.data, 2, 4)
+            );
+        case 5:
+            return (void)printf("%#06X if (V%x == V%x) - Skips the next instruction if VX does not equal NN (usually the next instruction is a jump to skip a code block).\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 2),
+                nibble_slice(instr.data, 2, 3)
+            );
+        case 6:
+            return (void)printf("%#06X V%x = %X - Sets VX to NN.\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 2),
+               nibble_slice(instr.data, 2, 4)
+            );
+        case 7:
+            return (void)printf("%#06X V%x += %X - Adds NN to VX (carry flag is not changed).\n",
+               instr.data,
+               nibble_slice(instr.data, 1, 2),
+               nibble_slice(instr.data, 2, 4)
+            );
+
+        case 8:
+
+            // check last nibble
+            //switch (nibble_slice(instr.data, 3, 4)) {
+            switch (instr.subtype) {
+                case 0:
+                    return (void)printf("%#06X V%x = V%x - Sets VX to the value of VY.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 1:
+                    return (void)printf("%#06X V%x |= V%x - Sets VX to VX or VY. (bitwise OR operation).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 2:
+                    return (void)printf("%#06X V%x &= V%x - Sets VX to VX and VY. (bitwise AND operation).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 3:
+                    return (void)printf("%#06X V%x ^= V%x - Sets VX to VX xor VY.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 4:
+                    return (void)printf("%#06X V%x += V%x - Adds VY to VX. VF is set to 1 when there's an overflow, and to 0 when there is not.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 5:
+                    return (void)printf("%#06X V%x -= V%x - VY is subtracted from VX. VF is set to 0 when there's an underflow, and 1 when there is not. (i.e. VF set to 1 if VX >= VY and 0 if not).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2),
+                        nibble_slice(instr.data, 2, 3)
+                    );
+                case 6:
+                    return (void)printf("%#06X V%x >>= 1 - Shifts VX to the right by 1, then stores the least significant bit of VX prior to the shift into VF.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 7: {
+                    uint8_t vx = nibble_slice(instr.data, 1, 2);
+                    return (void) printf(
+                            "%#06X V%x = V%x - V%x - Sets VX to VY minus VX. VF is set to 0 when there's an underflow, and 1 when there is not. (i.e. VF set to 1 if VY >= VX).\n",
+                            instr.data,
+                            vx,
+                            nibble_slice(instr.data, 2, 3), // Vy
+                            vx
+                    );
+                }
+                case 0xE:
+                    return (void)printf("%#06X V%x <<= 1 - Shifts VX to the left by 1, then sets VF to 1 if the most significant bit of VX prior to that shift was set, or to 0 if it was unset.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+            }
+
+        case 9:
+            return (void)printf("%#06X if (V%x != V%x) - Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block).\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 2),
+                nibble_slice(instr.data, 2, 3)
+            );
+
+        case 0xA:
+            return (void)printf("%#06X I = %#03X; - Sets I to the address NNN.\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 4)
+            );
+        case 0xB:
+            return (void)printf("%#06X PC = V0 + %#03X - Jumps to the address NNN plus V0.\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 4)
+            );
+
+        case 0xC:
+            return (void)printf("%#06X V%x = rand() & %#02X; - Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 2),
+                nibble_slice(instr.data, 2, 4)
+            );
+
+        case 0xD:
+            return (void)printf("%#06X draw(V%x, V%x, %x) - Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value does not change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that does not happen.\n",
+                instr.data,
+                nibble_slice(instr.data, 1, 2),
+                nibble_slice(instr.data, 2, 3),
+                nibble_slice(instr.data, 3, 4)
+            );
+
+        case 0xE:
+            switch (nibble_slice(instr.data, 2, 4)) {
+                case 0x9E:
+                    return (void)printf("%#06X if (key() == V%x) - Skips the next instruction if the key stored in VX(only consider the lowest nibble) is pressed (usually the next instruction is a jump to skip a code block).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0xA1:
+                    return (void)printf("%#06X if (key() != V%x) - Skips the next instruction if the key stored in VX(only consider the lowest nibble) is not pressed (usually the next instruction is a jump to skip a code block).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+            }
+        case 0xF:
+            switch (nibble_slice(instr.data, 2, 4)) {
+                case 0x07:
+                    return (void)printf("%#06X V%x = get_delay() - Sets VX to the value of the delay timer.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x0A:
+                    return (void)printf("%#06X V%x = get_key() - A key press is awaited, and then stored in VX (blocking operation, all instruction halted until next key event, delay and sound timers should continue processing).\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x15:
+                    return (void)printf("%#06X delay_timer(V%x) - Sets the delay timer to VX.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x18:
+                    return (void)printf("%#06X sound_timer(V%x) - Sets the sound timer to VX.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x1E:
+                    return (void)printf("%#06X I += V%x - Adds VX to I. VF is not affected.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x29:
+                    return (void)printf("%#06X I = sprite_addr[V%x] - Sets I to the location of the sprite for the character in VX(only consider the lowest nibble). Characters 0-F (in hexadecimal) are represented by a 4x5 font.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x33:
+                    return (void)printf(
+                            "%#06X set_BCD(V%x) "
+                        "*(I+0) = BCD(3);"
+                        "*(I+1) = BCD(2);"
+                        "*(I+2) = BCD(1); - Stores the binary-coded decimal representation of VX, with the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x55:
+                    return (void)printf("%#06X reg_dump(V%x, &I)  - Stores from V0 to VX (including VX) in memory, starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+                case 0x65:
+                    return (void)printf("%#06X reg_load(V%x, &I) - Fills from V0 to VX (including VX) with values from memory, starting at address I. The offset from I is increased by 1 for each value read, but I itself is left unmodified.\n",
+                        instr.data,
+                        nibble_slice(instr.data, 1, 2)
+                    );
+            }
+
+        default:
+            printf("NOT AN OPCODE: %#06X - b:%x,%x\n",
+                instr.data, instr.byte[0], instr.byte[1]
+            );
+            //assert(0);
+
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+
+    assert(argc > 1);
+
+    //dump_instruction(0x6a00);
+    //return 0;
+
+    //dump_instruction(0x3230);
+    //dump_instruction(0x4230);
+    //dump_instruction(0x5230);
+    //dump_instruction((opcode_t){.data = 0X80FFu});
+
+    //return 0;
+
+    printf("argv[1] = \"%s\"\n", argv[1]);
+    FILE *file = fopen(argv[1], "rb");
+
+    for (opcode_t instr; fread(&instr.data, sizeof(uint16_t), 1, file) == 1; ) {
+        instr.data = be16toh(instr.data); // for my mental health
+        dump_instruction(instr);
+    }
+
+    if (ferror(file))
+        perror("fread()");
+
+    fclose(file);
+}
