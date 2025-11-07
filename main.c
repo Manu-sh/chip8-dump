a0e4355ef33b8679996b7f47ba5805d452e0ce3e
Manu-Sh <s3gmentationfault@gmail.com>
2025-11-07 13:26:03 +0100

    chip8 - sdl3

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
 #define _DEFAULT_SOURCE
-
-#include <stdio.h>
-#include <stdint.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include <bit_utility.h>
 #include <chip8.h>
 #include <font.h>
@@ -13,34 +6,66 @@
 #include <dbg.h>
 #include <scale.h>
 
-#if 1
-#include <raylib.h>
-
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_keyboard.h>
 
+
+SDL_Palette * sdl_palette_new() {
+
+    SDL_Palette *palette = SDL_CreatePalette(256);
+    SDL_Color colors[256];
+
+    memset(colors, 0x00, sizeof(colors)); // every other color than white is black
+    memset(colors + 255, 0xff, sizeof(colors[0])); // this is white
+
+    SDL_SetPaletteColors(palette, colors, 0, 256);
+
+    return palette;
+}
+
+void sdl_palette_free(SDL_Palette *palette) {
+    SDL_DestroyPalette(palette);
+}
 
 int main(int argc, char *argv[]) {
 
     assert(argc > 1);
     printf("argv[1] = \"%s\"\n", argv[1]);
 
-    SetTraceLogLevel(LOG_ERROR);
+    //SetTraceLogLevel(LOG_ERROR);
+    //InitWindow(SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE, "chip8 emulator");
+    //SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-    InitWindow(SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE, "chip8 emulator");
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //SetTargetFPS(60 * 12); // Set our game to run at 60 frames-per-second
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "chip8 emulator",
+        SCREEN_WIDTH  * SCALE,
+        SCREEN_HEIGHT * SCALE,
+        SDL_WINDOW_RESIZABLE
+    );
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    SDL_SetRenderVSync(renderer, SDL_RENDERER_VSYNC_ADAPTIVE);
 
-    Texture2D tex = {
-        .id      = rlLoadTexture(NULL, GetScreenWidth(),  GetScreenHeight(), PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, 1),
-        .width   = GetScreenWidth(),
-        .height  = GetScreenHeight(),
-        .format  = PIXELFORMAT_UNCOMPRESSED_GRAYSCALE,
-        .mipmaps = 1
-    };
+    SDL_Surface *surface = SDL_CreateSurface(
+        SCREEN_WIDTH  * SCALE,
+        SCREEN_HEIGHT * SCALE,
+        SDL_PIXELFORMAT_INDEX8
+    );
 
+    SDL_Palette *palette = sdl_palette_new();
+    SDL_SetSurfacePalette(surface, palette);
 
     assert(argc > 1);
 
@@ -49,15 +74,29 @@ int main(int argc, char *argv[]) {
     if (!chip_load_rom(chip, argv[1]))
         goto die;
 
-    //exec(chip, *(opcode_t *)(chip->memory + chip->PC));
-    //exec(chip, *(opcode_t *)(chip->prog_beg));
+    SDL_Event event;
 
-    // Main game loop
-    while (!WindowShouldClose()) { // Detect window close button or ESC key
+    while (1) {
 
-        PollInputEvents();
+        while (SDL_PollEvent(&event)) {
 
-        /* for (int k; (k = GetKeyPressed()) != 0; ) {
+            if (event.type == SDL_EVENT_QUIT)
+                goto die;
+
+            if (event.type != SDL_EVENT_KEY_DOWN || event.key.repeat)
+                continue;
+
+            switch (event.key.scancode) {
+                case SDL_SCANCODE_0: chip->keypad[HKEY_0] = PRESS; break;
+                case SDL_SCANCODE_1: chip->keypad[HKEY_1] = PRESS; break;
+            }
+
+            printf("some key pressed\n");
+            sleep(1);
+        }
+
+/*
+        for (int k; (k = GetKeyPressed()) != 0; ) {
             switch (k) {
                 case KEY_ZERO:  chip->keypad[HKEY_0] = PRESS; break;
                 case KEY_ONE:   chip->keypad[HKEY_1] = PRESS; break;
@@ -76,108 +115,64 @@ int main(int argc, char *argv[]) {
                 case KEY_E:     chip->keypad[HKEY_E] = PRESS; break;
                 case KEY_F:     chip->keypad[HKEY_F] = PRESS; break;
             }
-        }*/
-
-
-        if (IsKeyDown(KEY_ZERO))  chip->keypad[HKEY_0] = PRESS;
-        if (IsKeyDown(KEY_ONE))   chip->keypad[HKEY_1] = PRESS;
-        if (IsKeyDown(KEY_TWO))   chip->keypad[HKEY_2] = PRESS;
-        if (IsKeyDown(KEY_THREE)) chip->keypad[HKEY_3] = PRESS;
-        if (IsKeyDown(KEY_FOUR))  chip->keypad[HKEY_4] = PRESS;
-        if (IsKeyDown(KEY_FIVE))  chip->keypad[HKEY_6] = PRESS;
-        if (IsKeyDown(KEY_SIX))   chip->keypad[HKEY_7] = PRESS;
-        if (IsKeyDown(KEY_SEVEN)) chip->keypad[HKEY_8] = PRESS;
-        if (IsKeyDown(KEY_EIGHT)) chip->keypad[HKEY_9] = PRESS;
-        if (IsKeyDown(KEY_A))     chip->keypad[HKEY_A] = PRESS;
-        if (IsKeyDown(KEY_B))     chip->keypad[HKEY_B] = PRESS;
-        if (IsKeyDown(KEY_C))     chip->keypad[HKEY_C] = PRESS;
-        if (IsKeyDown(KEY_D))     chip->keypad[HKEY_D] = PRESS;
-        if (IsKeyDown(KEY_E))     chip->keypad[HKEY_E] = PRESS;
-        if (IsKeyDown(KEY_F))     chip->keypad[HKEY_F] = PRESS;
-
-
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        }
+*/
+        // key_states[SDL_SCANCODE_W]
 
         //dbg("PC: %#04x ", chip->PC);
         chip_exec(chip, chip_fetch(chip, chip->PC));
 
-        // Draw
-        BeginDrawing();
 
-        //ClearBackground(BLACK);
 
-        // draw a line of pixel
-        //memset(chip->screen + 2 * 64, 0xff, 64);
+        // Copia il framebuffer dentro la surface
+        SDL_LockSurface(surface);
+        memcpy(surface->pixels, scale_screen(chip), SCREEN_WIDTH * SCALE * SCREEN_HEIGHT * SCALE);
+        SDL_UnlockSurface(surface);
 
-        //UpdateTexture(tex, scale_chip_screen(chip) );
-        UpdateTexture(tex, scale_screen(chip));
 
-        // disegna la texture con scaling automatico
-        DrawTexture(tex, 0, 0, WHITE);
+        // Aggiorna la texture con il contenuto del framebuffer
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-        //DrawText("ciao!", 0, 0, 20, WHITE);
-        EndDrawing();
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+        SDL_DestroyTexture(texture); // rilascia texture temporanea
 
         printf("%s\n", byte_dump(chip->keypad, sizeof(chip->keypad)));
 
-        //usleep(200 * 1000);
+        //usleep(10 * 1000);
         chip_tick(chip);
-
         memset(chip->keypad, NOT_PRESS, sizeof(chip->keypad));
     }
 
+    /* for (int k; (k = GetKeyPressed()) != 0; ) {
+        switch (k) {
+            case KEY_ZERO:  chip->keypad[HKEY_0] = PRESS; break;
+            case KEY_ONE:   chip->keypad[HKEY_1] = PRESS; break;
+            case KEY_TWO:   chip->keypad[HKEY_2] = PRESS; break;
+            case KEY_THREE: chip->keypad[HKEY_3] = PRESS; break;
+            case KEY_FOUR:  chip->keypad[HKEY_4] = PRESS; break;
+            case KEY_FIVE:  chip->keypad[HKEY_5] = PRESS; break;
+            case KEY_SIX:   chip->keypad[HKEY_6] = PRESS; break;
+            case KEY_SEVEN: chip->keypad[HKEY_7] = PRESS; break;
+            case KEY_EIGHT: chip->keypad[HKEY_8] = PRESS; break;
+            case KEY_NINE:  chip->keypad[HKEY_9] = PRESS; break;
+            case KEY_A:     chip->keypad[HKEY_A] = PRESS; break;
+            case KEY_B:     chip->keypad[HKEY_B] = PRESS; break;
+            case KEY_C:     chip->keypad[HKEY_C] = PRESS; break;
+            case KEY_D:     chip->keypad[HKEY_D] = PRESS; break;
+            case KEY_E:     chip->keypad[HKEY_E] = PRESS; break;
+            case KEY_F:     chip->keypad[HKEY_F] = PRESS; break;
+        }
+    }*/
+
 die:
-    CloseWindow(); // Close window and OpenGL context
+    // Close window and OpenGL context
+    SDL_DestroySurface(surface);
+    sdl_palette_free(palette);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     chip_free(chip);
     return 0;
 }
-
-#else
-int main(int argc, char *argv[]) {
-
-    assert(argc > 1);
-
-    //dump_instruction(0x6a00);
-    //return 0;
-
-    //dump_instruction(0x3230);
-    //dump_instruction(0x4230);
-    //dump_instruction(0x5230);
-    //dump_instruction((opcode_t){.data = 0X80FFu});
-
-    //return 0;
-
-    printf("argv[1] = \"%s\"\n", argv[1]);
-
-#if 1
-    FILE *file = fopen(argv[1], "rb");
-
-    for (opcode_t instr; fread(&instr.data, sizeof(uint16_t), 1, file) == 1; ) {
-        instr.data = be16toh(instr.data); // for my mental health
-        dump_instruction(instr);
-    }
-
-    if (ferror(file))
-        perror("fread()");
-
-    fclose(file);
-	return 0;
-#endif
-
-    size_t file_sz, mem_sz;
-    uint8_t *rom = rom_map(argv[1], &file_sz, &mem_sz);
-    assert(rom);
-    assert( ((uintptr_t)rom) % sizeof(uint16_t) == 0);
-
-    printf("file size in bytes: %zu\n", file_sz);
-    printf("mem size in bytes: %zu\n", mem_sz);
-
-
-    //
-
-    free(rom);
-}
-#endif
